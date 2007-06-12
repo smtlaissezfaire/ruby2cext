ed8bb5b76f5e13d2b6a25dca452ea8c06d0bf4f7
dbat <dbat@fb9c67ac-1a16-0410-bc3f-e42715d93028>
2007-06-12 02:29:27 +0000

    added Ruby 1.8.5-p35, 1.8.5-p52, 1.8.6 and 1.8.6-p36 to SUPPORTED_RUBY_VERSIONS
    git-svn-id: http://ruby2cext.rubyforge.org/svn/trunk@47 fb9c67ac-1a16-0410-bc3f-e42715d93028

--- lib/ruby2cext/version.rb
+++ lib/ruby2cext/version.rb
@@ -7,12 +7,13 @@ module Ruby2CExtension
 
 	SUPPORTED_RUBY_VERSIONS = {
 		"1.8.4" => ["2005-12-24"],
-		"1.8.5" => ["2006-08-25", "2006-12-04", "2006-12-25"],
+		"1.8.5" => ["2006-08-25", "2006-12-04", "2006-12-25", "2007-03-13", "2007-06-07"],
+		"1.8.6" => ["2007-03-13", "2007-06-07"],
 	}
 
 	if (dates = SUPPORTED_RUBY_VERSIONS[RUBY_VERSION])
 		unless dates.include? RUBY_RELEASE_DATE
-			warn "Ruby2CExtension warning: ruby #{RUBY_VERSION} (#{RUBY_RELEASE_DATE}) is not the official release of Ruby #{RUBY_VERSION}, there might be problems"
+			warn "Ruby2CExtension warning: ruby #{RUBY_VERSION} (#{RUBY_RELEASE_DATE}) is an unknown release of Ruby #{RUBY_VERSION}, there might be problems"
 		end
 	else
 		warn "Ruby2CExtension warning: ruby #{RUBY_VERSION} (#{RUBY_RELEASE_DATE}) is not supported by Ruby2CExtension #{VERSION}"
